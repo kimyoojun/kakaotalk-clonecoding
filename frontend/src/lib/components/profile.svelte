98d29d9855c4329b4a6632d4909eb98427ea8a0e
kim yoo jun <anano3134@gmail.com>
2024-03-11 16:21:26 +0900

    프로필 컴포넌트 생성

--- frontend/src/lib/components/profile.svelte
+++ frontend/src/lib/components/profile.svelte
@@ -1,7 +1,62 @@
-<div class="profile"></div>
+<script>
+    export let profileImg = ""
+    export let profileName = ""
+    export let profileMessage = ""
+</script>
+
+<button class="profile-wrap">
+    <div class="img-wrap">
+        <img src={profileImg} alt="Profile" id="profile-img"/>
+    </div>
+    <div class="status-message-wrap">
+        <h4 class="profile-name">{profileName}</h4>
+        <div class="status-message">{profileMessage}</div>
+    </div>
+</button>
 
 <style>
-    .profile {
+    .profile-wrap {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        background-color: transparent;
+        border: none;
+        padding: 0;
+    }
+
+    .profile-wrap:hover {
+        background-color: rgb(240, 240, 240);
+    }
+    
+    .img-wrap {
+        width: 70px;
         height: 100%;
+        display: flex;
+        align-items: center;
+    }
+
+    #profile-img {
+        width: 40px;
+        height: 40px;
+        border-radius: 16px;
+        margin-left: 20px;
+    }
+
+    .status-message-wrap {
+        height: 100%;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+    }
+
+    .profile-name {
+        font-size: 0.85rem;
+        margin: 0;
+    }
+
+    .status-message {
+        font-size: 0.7rem;
     }
 </style>
