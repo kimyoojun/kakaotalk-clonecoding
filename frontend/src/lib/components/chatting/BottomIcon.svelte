d6c5836801f50cbac1e393cf51cc0bc14e7912b6
kim yoo jun <anano3134@gmail.com>
2024-03-11 16:22:12 +0900

    채팅창 하단 아이콘 컴포넌트 생성

---
+++ frontend/src/lib/components/chatting/BottomIcon.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let textBottomIcon = ""
+</script>
+
+<button class="text-icon">
+    <img src={textBottomIcon} alt="Icon" id="text-icon"/>
+</button>
+
+<style>
+    .text-icon {
+        width: 30px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        background-color: transparent;
+    }
+
+    #text-icon {
+        width: 20px;
+    }
+</style>
