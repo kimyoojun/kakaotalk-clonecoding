5272fb3f64974e9947b942be20dce75e6abe80ec
kim yoo jun <anano3134@gmail.com>
2024-03-11 16:22:26 +0900

    채팅창 상단 아이콘 컴포넌트 생성

---
+++ frontend/src/lib/components/chatting/TopIcon.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let chatTopIcon = ""
+</script>
+
+<button class="chat-top-icon">
+    <img src={chatTopIcon} alt="Icon" id="chatting-icon"/>
+</button>
+
+<style>
+    .chat-top-icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        background-color: transparent;
+    }
+
+    #chatting-icon {
+        width: 20px;
+    }
+</style>
