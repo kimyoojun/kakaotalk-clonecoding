8770e79f4e66f30aaf7caa59f2e9034dcc6d9582
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-14 16:24:08 +0900

    cors설정을및 db초기화

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.message_box import message_box
+from db.database import init_db
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5173/*",
+]
+
+app.add_middleware( 
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(message_box.router)
+
+@app.get("/")
+def read_root():
+    return {"연결상태": "true"}
+
+if __name__ == "__main__":
+    init_db()
+
