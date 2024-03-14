88a590b1d413d1afa927d379f6d4343c8221e837
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-14 16:24:25 +0900

    데이터베이스가 없다면 생성

--- db/database.py
+++ db/database.py
@@ -1,12 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./kakaotalk-clonecoding.db"
+engine = create_engine("sqlite:///database.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def init_db():
+  SQLModel.metadata.create_all(engine)
 
-Base = declarative_base()
