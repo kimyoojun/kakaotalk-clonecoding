ad6d576a8c483e7fa1bd06088d3f19cfb5e2a72f
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:47:36 +0900

    회원가입 페이지 생성

--- frontend/src/routes/(lending)/+page.svelte
+++ frontend/src/routes/(lending)/+page.svelte
@@ -1,1 +1,74 @@
-<h1>카카오톡 클론코딩 1일차</h1>
+<script>
+  import InputBox from "$lib/components/InputBox.svelte"
+  import Btn from "$lib/components/btn.svelte"
+
+</script>
+<div class="login-wrap">
+  <div class="title-wrap">
+    <h1 class="title">코코아톡</h1>
+  </div>
+  <div class="input-wrap">
+    <InputBox inputName="아이디"/>
+    <InputBox inputName="비밀번호"/>
+  </div>
+  <div class="btn-wrap">
+    <Btn btnName="로그인"/>
+  </div>
+  <div class="register-go-wrap">
+    <a href="/register" class="register-go">회원가입</a>
+  </div>
+</div>
+
+<style>
+  .login-wrap {
+    width: 100%;
+    height: 100%;
+    background-color: #f9e000;
+  }
+
+  .title-wrap {
+    width: 100%;
+    height: 300px;
+    display: flex;
+    align-items: flex-end;
+  }
+
+  .title {
+    width: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+  }
+
+  .input-wrap {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .btn-wrap {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .register-go-wrap {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .register-go {
+    text-decoration-line: none;
+    color: rgb(60, 60, 60);
+    font-size: 0.8rem;
+  }
+</style>
