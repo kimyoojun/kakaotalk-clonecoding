4691f5f09cf12bfa8878c755cd476486ca4a20c6
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:46:12 +0900

    회원가입 라우터 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routes.message_box import message_box
 from db.database import init_db
+from routes.auth import register
 
 app = FastAPI()
 
@@ -20,6 +21,7 @@ app.add_middleware(
 )
 
 app.include_router(message_box.router)
+app.include_router(register.router)
 
 @app.get("/")
 def read_root():
