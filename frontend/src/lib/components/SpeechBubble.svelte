8608961722c98f0e80493ba8294955db509dc09e
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:49:27 +0900

    말풍선 생성

---
+++ frontend/src/lib/components/SpeechBubble.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let mySpeechBubble = ""
+</script>
+
+<div class="speech-wrap">
+  <div class="speech">
+    {mySpeechBubble}
+  </div>
+</div>
+
+<style>
+  .speech-wrap {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .speech {
+    background-color: #fef01b;
+    margin-right: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    height: auto;
+    padding: 8px 8px;
+    border-radius: 4px;
+  }
+</style>
