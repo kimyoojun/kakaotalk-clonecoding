8b22d4d20c3d2524384817da566366467f3ae9e1
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:46:22 +0900

    버튼 컴포넌트 생성

---
+++ frontend/src/lib/components/btn.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let btnName = ""
+  export let onClick
+
+  const handleClick = () => {
+    onClick()
+  }
+</script>
+
+<button class="btn-box" on:click={handleClick}>{btnName}</button>
+
+<style>
+  .btn-box {
+    width: 252px;
+    height: 44px;
+    font-size: 1.2rem;
+  }
+</style>
+
+
+
