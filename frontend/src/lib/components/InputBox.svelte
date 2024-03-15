e10966748c41dba4efe21b3359740ac0adc62d35
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:46:42 +0900

    input 컴포넌트 생성

---
+++ frontend/src/lib/components/InputBox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let inputName = ""
+  export let value = ""
+</script>
+
+<input class="input-box" placeholder={inputName} bind:value={value}/>
+
+<style>
+  .input-box {
+    width: 252px;
+    height: 36px;
+    border: 0.5px solid rgb(202, 202, 202);
+    outline: none;
+    padding: 0 10px;
+  }
+</style>
