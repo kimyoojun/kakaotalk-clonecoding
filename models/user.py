dd592bbdac1b8b28e3b302e0f9179c9f461377d3
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-15 17:48:22 +0900

    유저 테이블 생성

---
+++ models/user.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+  name: str = Field(index=True)
+  email: str = Field(unique=True)
+  id: str = Field(primary_key=True, index=True, unique=True)
+  pw: str
