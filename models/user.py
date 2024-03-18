8526ecd2b656ad419c5895b72a261878948742e3
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-19 08:57:37 +0900

    토큰 추가 및 유니크키 삭제

--- models/user.py
+++ models/user.py
@@ -3,5 +3,6 @@ from sqlmodel import SQLModel, Field
 class User(SQLModel, table=True):
   name: str = Field(index=True)
   email: str = Field(unique=True)
-  id: str = Field(primary_key=True, index=True, unique=True)
+  id: str = Field(primary_key=True, index=True)
   pw: str
+  token: str
