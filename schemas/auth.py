2a3aecf11bad2c0e5bfd57a79f3db84280664364
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-19 08:58:29 +0900

    로그인 스키마 생성

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class IRegister(BaseModel):
   name: str = Field(...)
   email: str = Field(...)
+  id: str = Field(...)
+  pw: str = Field(...)
+  token: Optional[str] = None 
+
+class ILogin(BaseModel):
   id: str = Field(...)
   pw: str = Field(...)
