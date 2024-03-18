d25f7a29a07f1a10cb2fa0caac6357abafeb7462
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-19 08:56:45 +0900

    데이터 베이스 제거 함수 생성

--- db/database.py
+++ db/database.py
@@ -2,6 +2,10 @@ from sqlmodel import SQLModel, create_engine
 
 engine = create_engine("sqlite:///database.db")
 
-def init_db():
+async def init_db():
   SQLModel.metadata.create_all(engine)
+
+
+async def drop_db():
+  SQLModel.metadata.drop_all(engine)
 
