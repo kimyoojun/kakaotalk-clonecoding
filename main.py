2bc7137aa0bc757c74d80079fdc4698cd5c4a925
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-19 08:56:30 +0900

    fastapi실행하고 끌때 데이터베이스 생성및 삭제

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes.message_box import message_box
-from db.database import init_db
 from routes.auth import register
+from routes.auth import login
+from db.database import init_db, drop_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    print("API starting up")
+    await init_db()
+    yield
+    print("API shutting down")
+    await drop_db()
+
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost:5173",
@@ -22,11 +33,14 @@ app.add_middleware(
 
 app.include_router(message_box.router)
 app.include_router(register.router)
+app.include_router(login.router)
 
 @app.get("/")
 def read_root():
     return {"연결상태": "true"}
 
-if __name__ == "__main__":
-    init_db()
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
