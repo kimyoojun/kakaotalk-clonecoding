ce58568af2bf9593d05c05e913daefb7095a15ad
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-19 08:59:23 +0900

    로그인 api 연결및 로그인시 토큰 받음

--- frontend/src/routes/(lending)/+page.svelte
+++ frontend/src/routes/(lending)/+page.svelte
@@ -1,6 +1,22 @@
 <script>
   import InputBox from "$lib/components/InputBox.svelte"
-  import Btn from "$lib/components/btn.svelte"
+  import Btn from "$lib/components/Btn.svelte"
+  import axios from "axios";
+  import { userName, accessToken, isLogin } from "$lib/store/login.js";
+
+  let loginId = ""
+  let loginPw = ""
+
+  const loginBtn = async () => {
+    const loginAPI = await axios.post("http://127.0.0.1:8000/auth/login", {"id": loginId, "pw": loginPw})
+    
+    if (loginAPI.status == 200){
+      window.location.href="/app/friend"
+      $userName = loginAPI.data[1].name
+      $accessToken = loginAPI.data[1].token
+      $isLogin = true
+    }
+  }
 
 </script>
 <div class="login-wrap">
@@ -8,11 +24,11 @@
     <h1 class="title">코코아톡</h1>
   </div>
   <div class="input-wrap">
-    <InputBox inputName="아이디"/>
-    <InputBox inputName="비밀번호"/>
+    <InputBox inputName="아이디" bind:value={loginId}/>
+    <InputBox inputName="비밀번호" bind:value={loginPw}/>
   </div>
   <div class="btn-wrap">
-    <Btn btnName="로그인"/>
+    <Btn btnName="로그인" onClick={loginBtn}/>
   </div>
   <div class="register-go-wrap">
     <a href="/register" class="register-go">회원가입</a>
