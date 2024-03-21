bd81420e04b520af2c5bdc6473eab94f969be3b9
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-21 10:42:53 +0900

    main에 친구 추가 API추가

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.message_box import message_box
 from routes.auth import register
 from routes.auth import login
-from db.database import init_db, drop_db
+from db.database import init_db
+from routes.friend_add import friend_add
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
     print("API starting up")
     await init_db()
     yield
-    print("API shutting down")
-    await drop_db()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -34,6 +33,7 @@ app.add_middleware(
 app.include_router(message_box.router)
 app.include_router(register.router)
 app.include_router(login.router)
+app.include_router(friend_add.router)
 
 @app.get("/")
 def read_root():
