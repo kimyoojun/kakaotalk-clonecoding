0da74e81e0206bc4f6407a5b8194631887eb8e47
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-22 14:50:53 +0900

    User테이블에 chats을 list형식으로 변경

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 from sqlmodel import SQLModel, Field, Column, JSON
 from typing import List,Optional
+import uuid
 
 class User(SQLModel, table=True):
+  # uuid: str = Field(unique=True, index=True)
   name: str = Field(index=True)
   email: str = Field(unique=True)
   id: str = Field(primary_key=True, index=True)
   pw: str
   token: str
   friends: List[str] = Field(sa_column=Column(JSON))
-  chats: Optional[str] = None
+  chats: List[str] = Field(sa_column=Column(JSON))
