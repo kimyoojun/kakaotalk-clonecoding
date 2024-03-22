2a51a7cf56109dbb55fd0901e2feba992f3896f9
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-22 17:30:40 +0900

    필요없는코드 삭제 및 수정, uuid추가

--- models/user.py
+++ models/user.py
@@ -1,12 +1,11 @@
 from sqlmodel import SQLModel, Field, Column, JSON
-from typing import List,Optional
-import uuid
+from typing import List
 
 class User(SQLModel, table=True):
-  # uuid: str = Field(unique=True, index=True)
+  uuid: str = Field(primary_key=True, index=True)
   name: str = Field(index=True)
   email: str = Field(unique=True)
-  id: str = Field(primary_key=True, index=True)
+  id: str = Field(unique=True, index=True)
   pw: str
   token: str
   friends: List[str] = Field(sa_column=Column(JSON))
