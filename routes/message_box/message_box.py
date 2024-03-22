cce4fa343dc497de385a2cf95e81d7c591555c4c
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-22 17:31:00 +0900

    채팅창 생성

--- routes/message_box/message_box.py
+++ routes/message_box/message_box.py
@@ -1,14 +1,50 @@
 from fastapi import APIRouter
-from sqlmodel import Session, select
+from sqlmodel import Session, select, update
 from starlette.responses import JSONResponse as JSON
+import uuid
 
 from db.database import engine
 from models.message import Message
-from schemas.speech import IMessage
+from models.user import User
+from schemas.message import IMessage
 
 router = APIRouter(prefix="/message", tags=["message"])
 
 @router.post("/")
+async def chatting(req:IMessage):
+  with Session(engine) as session:
+    useruuid = select(User).where(User.name == req.user_name)
+    user_uuid_infrom = session.exec(useruuid)
+    user = user_uuid_infrom.one()
+
+    if Message.participation_uuid != req.my_uuid and Message.participation_uuid != user.uuid:
+
+      chat_uuid = uuid.uuid1()
+
+      participation = []
+      participation.append(req.my_uuid)
+      participation.append(user.uuid)
+
+      message = Message(
+        uuid=str(chat_uuid),
+        participation_uuid=participation
+      )
+
+      my_chats_add = update(User).where(User.uuid == req.my_uuid).values(chats=str(chat_uuid))
+      user_chats_add = update(User).where(User.uuid == user.uuid).values(chats=str(chat_uuid))
+
+      session.add(message)
+      session.exec(my_chats_add)
+      session.exec(user_chats_add)
+      session.commit()
+  return message
+    # my_chatting = select(Message).where(Message.participation_uuid == req.my_uuid and Message.participation_uuid == req.user_uuid)
+    # chat_infrom = session.exec(my_chatting)
+    # chat = chat_infrom.one()
+
+
+
+@router.post("/send")
 async def my_message(req: IMessage):
   if not req.message:
     return JSON({"msg": "메세지를 입력해주세요."}, 400)
