f7b07befab8dc5a008c9d0187d4c15a468ab9401
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-25 18:19:06 +0900

    채팅창 접속시 이전 대화 기록 나열,  메세지 전송시 백엔드에 추가 및 필요없는 코드 제거

--- routes/message_box/message_box.py
+++ routes/message_box/message_box.py
@@ -6,29 +6,37 @@ import uuid
 from db.database import engine
 from models.message import Message
 from models.user import User
-from schemas.message import IMessage
+from schemas.message_create import IMessage_create
+from schemas.message_goin import IMessage_goin
+from schemas.message_send import IMessage_send
 
 router = APIRouter(prefix="/message", tags=["message"])
 
 @router.post("/")
-async def chatting(req:IMessage):
+async def chatting(req:IMessage_create):
   with Session(engine) as session:
     useruuid = select(User).where(User.name == req.user_name)
     user_uuid_infrom = session.exec(useruuid)
     user = user_uuid_infrom.first()
 
 
-
     is_user = select(Message).where(Message.user_list == [req.my_uuid, user.uuid])
     is_uesrlist = session.exec(is_user)
     userlist = is_uesrlist.first()
+
 
     is_users = select(Message).where(Message.user_list == [user.uuid, req.my_uuid])
     is_uesrslist = session.exec(is_users)
     userslist = is_uesrslist.first()
     
+    if userlist:
+      chatuuid = userlist.uuid
+
+    if userslist:
+      chatuuid = userslist.uuid
+
   if userlist or userslist:
-    return "존재함"
+    return "존재함", user, chatuuid
   else:
     chat_uuid = uuid.uuid1()
 
@@ -37,7 +45,6 @@ async def chatting(req:IMessage):
     participation.append(user.uuid)
 
     message = Message(
-      message='a',
       uuid=str(chat_uuid),
       user_list=participation
     )
@@ -49,30 +56,26 @@ async def chatting(req:IMessage):
     session.exec(my_chats_add)
     session.exec(user_chats_add)
     session.commit()
-    return "채팅창 생성"
+    return "채팅창 생성", chat_uuid
     
 
 
 
-
-    # my_chatting = select(Message).where(Message.participation_uuid == req.my_uuid and Message.participation_uuid == req.user_uuid)
-    # chat_infrom = session.exec(my_chatting)
-    # chat = chat_infrom.one()
-
-
-
 @router.post("/send")
-async def my_message(req: IMessage):
-  if not req.message:
-    return JSON({"msg": "메세지를 입력해주세요."}, 400)
-  
-  message = Message(
-    message = req.message
-  )
+async def my_message(req: IMessage_send):
+  with Session(engine) as session:
+    select_chatting = select(Message).where(Message.uuid == req.chatuuid)
+    chatting_inform = session.exec(select_chatting).first()
 
+    if chatting_inform.message is None:
+      newChat = []
+    else:
+      newChat = chatting_inform.message
+    newChat.append(req.message)
+      
   try:
-    with Session(engine) as session:
-      session.add(message)
+      update_chat = update(Message).where(Message.uuid == req.chatuuid).values(message = newChat)
+      session.exec(update_chat)
       session.commit()
   except Exception as e:
     print(e)
@@ -80,11 +83,16 @@ async def my_message(req: IMessage):
   finally:
     return JSON({"msg": "메세지전송에 성공하였습니다"}, 200)
   
-@router.get("/window/")
-async def message_window():
+
+
+
+@router.post("/window")
+async def message_window(req: IMessage_goin):
   with Session(engine) as session:
-    message_table = select(Message)
-    message_value = session.exec(message_table).all()
-    last_message = message_value[-1]
-    print(last_message)
-  return last_message
+    user_select = select(User).where(User.uuid == req.useruuid)
+    user_infrom = session.exec(user_select).first()
+
+    chatuuid = select(Message).where(Message.uuid == req.chatuuid)
+    chat_record = session.exec(chatuuid).first()
+
+  return user_infrom.name, chat_record
