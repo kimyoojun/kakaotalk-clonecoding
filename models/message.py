d12e8e40f4a35ccd7cc1efeb94a9488640a74398
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-26 13:42:37 +0900

    메세지 전송 스키마 추가

--- models/message.py
+++ models/message.py
@@ -1,7 +1,10 @@
 from sqlmodel import SQLModel, Field, Column, JSON
-from typing import List, Optional
+from typing import List
 
 class Message(SQLModel, table=True):
+  create_user: str
   uuid: str = Field(primary_key=True, index=True)
   message: List[str] = Field(sa_column=Column(JSON))
   user_list: List[str] = Field(sa_column=Column(JSON))
+  my_msg: List[int] = Field(sa_column=Column(JSON))
+  user_msg: List[int] = Field(sa_column=Column(JSON))
