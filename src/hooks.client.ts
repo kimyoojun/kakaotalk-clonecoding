6bbe4983775db7bd53340c05125f197578c4f32e
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-29 16:08:14 +0900

    31231313

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,3 +2,4 @@ import axios from 'axios'
 
 axios.defaults.baseURL =
   'https://port-0-kokoatalk-api-44rk2blu9bhspu.sel5.cloudtype.app/'
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
