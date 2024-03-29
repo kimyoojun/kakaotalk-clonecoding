90a20431a35d8ffce64ee8ca3327bd4743293116
kimyoojun <109792486+kimyoojun@users.noreply.github.com>
2024-03-29 15:52:57 +0900

    adasdas

--- src/routes/chatting/+page.svelte
+++ src/routes/chatting/+page.svelte
@@ -17,7 +17,7 @@
     let ws
 
     onMount(async () => {
-        ws = new WebSocket('ws://localhost:8000/ws')
+        ws = new WebSocket('ws://port-0-kokoatalk-44rk2blu9bhspu.sel5.cloudtype.app/ws')
         chatUserUuid = localStorage.getItem("chatuseruuid")
         chatUuid = localStorage.getItem("chatuuid")
         myuuid = localStorage.getItem("myuuid")
